a86e494ea70035034abcfa3fb979bbb0d3c96bfb
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2015-10-21 02:37:07 -0400

    Map over board and pass the conent to the pieces.

--- web/src/components/Board.js
+++ web/src/components/Board.js
@@ -4,14 +4,17 @@ import Piece from './Piece';
 export default class Board extends React.Component {
   render(){
 
+    let {action, board, player} = this.props;
+
     let styles = {
       position: 'relative',
       width: '150px',
       margin: '0px auto'
     }
 
-    let pieces = [1,2,3,4,5,6,7,8,9].map((e, i)=> {
-      return (<Piece key={i} action={this.props.action} index={i} />);
+    let pieces = board.map((e, i)=> {
+      let content = e.isEmpty ? '' : e.player;
+      return (<Piece key={i} action={action} index={i} content={content} />);
     });
 
     return (<div style={styles}>{pieces}</div>);
