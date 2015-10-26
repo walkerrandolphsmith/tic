02686ce7d8d87960110855b32ec46da74193bba9
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2015-10-25 23:31:04 -0400

    Added react native directory.

---
+++ react-native/README.md
@@ -0,0 +1,5 @@
+#React Native Starter.
+
+##RUn
+Open `react-native/ios/App.xcodeproj` in xcode. Press play.
+run `npm start` in terminal in `react-native` directory.
---
+++ react-native/src/main.android.js
@@ -0,0 +1,6 @@
+'use strict';
+
+import {AppRegistry} from 'react-native';
+import GameComponent from './components/Game';
+
+AppRegistry.registerComponent('App', () => GameComponent);
