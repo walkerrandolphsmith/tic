ca25c615887ecd7c4b9b06f5a758fedf5e763ce8
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2015-11-19 21:01:04 -0500

    Update build

--- web/gulpfile.js
+++ web/gulpfile.js
@@ -1,19 +1,52 @@
-var gulp = require('gulp')
-var shell = require('gulp-shell')
-var connect = require('gulp-connect')
+'use strict';
 
-gulp.task('default', [
-  'server',
-  'webpack',
-])
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var config = require('./webpack.config');
+var webpack = require('webpack');
+var WebpackDevServer = require('webpack-dev-server');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var path = require('path');
+var _ = require('lodash');
+var promise = require('bluebird');
 
-gulp.task('server', function() {
-  connect.server({
-    root: 'app',
-    port: 3000
-  })
-})
+gulp.task('dev', [], function () {
+	pack(true)
+		.then(function (packingBundle) {
+			packingBundle.config.plugins = _.reject(packingBundle.config.plugins, function (plugin) {
+				return plugin instanceof HtmlWebpackPlugin;
+			});
+			var compiler = webpack(packingBundle.config);
+			new WebpackDevServer(compiler, {
+					hot: true,
+					contentBase: path.join(__dirname, './', packingBundle.config.output.contentBase),
+					stats: { colors: true }
+				})
+				.listen(3000, 'localhost', function (error) {
+					if (error) throw new gutil.PluginError('webpack-dev-server', error);
+					gutil.log('[webpack-dev-server]', 'http://localhost:3000/webpack-dev-server/index.html)');
+				});
+		});
+});
 
-gulp.task('webpack', shell.task([
-  './../node_modules/webpack/bin/webpack.js --watch --progress --colors'
-]))
+gulp.task('default', [ 'dev' ]);
+
+
+function pack(isDevelopment) {
+	if (isDevelopment) {
+		config.debug = true;
+		config.devtool = 'eval';
+	}
+	return new promise(function (resolve, reject) {
+		var compiler = webpack(config, function (error, stats) {
+			if (error) {
+				reject(error);
+			}
+			resolve({
+				config: config,
+				compiler: compiler,
+				stats: stats
+			});
+		});
+	});
+};
--- web/webpack.config.js
+++ web/webpack.config.js
@@ -1,24 +1,31 @@
 var path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   entry: {
     main: './src/main.js'
   },
-
   output: {
-    path: './app/',
+    path: path.join(__dirname, './dist'),
+		contentBase: 'dist',
     filename: "[name].js",
   },
+  plugins: [
+		new ExtractTextPlugin('styles.css')
+	],
   resolve: {
     root: [
       path.resolve(__dirname, './common'),
       path.resolve(__dirname, './src'),
       path.resolve(__dirname, './node_modules'),
     ],
+    extensions: ['', '.js', '.jsx'],
+		modulesDirectories: ['web_modules', 'node_modules', 'Components']
   },
   module: {
     loaders: [
       { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader", query: {stage: 0} },
+      { test: /\.less$/, loader: ExtractTextPlugin.extract('css?sourceMap!autoprefixer!less?sourceMap') },
     ]
   },
 };
